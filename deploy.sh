eff21ec598b0ac1b1d94a592a0d22f592a7423e3
matsuyoshi <sfbgwm30@gmail.com>
2019-01-24 00:00:23 +0900

    add favicon and deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Deploying updates to GitHub ..."
+
+# Build the project
+hugo -t hugo-zen
+
+# Go to public folder and add changes to git
+cd public
+git add .
+
+# commit changes
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+# Commit source repository changes
+git add .
+git commit -m "$msg"
+git push
